667d30ab3f8a3e7bead8ec4c2cabc69920d98ac5
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-17 22:49:22 -0300

    firts commit

---
+++ src/main/java/com/lirou/store/CliLirouStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lirou.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliLirouStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliLirouStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lirou/store/CliLirouStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirou.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliLirouStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
