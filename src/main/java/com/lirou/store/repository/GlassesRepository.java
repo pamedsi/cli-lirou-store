89d092e70b5fddba3229ed3924f4fdafe57c0988
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-24 11:20:01 -0300

    renames from 'Product' to 'Glasses'

--- src/main/java/com/lirou/store/repository/GlassesRepository.java
+++ src/main/java/com/lirou/store/repository/GlassesRepository.java
@@ -1,9 +1,9 @@
 package com.lirou.store.repository;
 
-import com.lirou.store.models.Product;
+import com.lirou.store.entities.Glasses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Long, Product> {
+public interface GlassesRepository extends JpaRepository<Long, Glasses> {
 }
