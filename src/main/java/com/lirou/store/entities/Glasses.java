981d983d05c11eaff7a71c6dc5cf2a3876d4e53d
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-24 10:59:27 -0300

    create classes entity

---
+++ src/main/java/com/lirou/store/entities/Glasses.java
@@ -0,0 +1,35 @@
+package com.lirou.store.entities;
+
+import com.lirou.store.DTOs.InputGlassesDTO;
+import com.lirou.store.models.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Getter
+@Setter
+public class Glasses extends Product {
+    @Column
+    private String model;
+    @Column
+    private String color;
+    @Column
+    private String brand;
+
+    public Glasses(InputGlassesDTO glassesDTO) {
+        setTitle(glassesDTO.title());
+        setPrice(glassesDTO.price());
+        setPic(glassesDTO.pic());
+        setInStock(glassesDTO.inStock());
+        setColor(glassesDTO.color());
+        setModel(glassesDTO.model());
+        setBrand(glassesDTO.brand());
+    }
+}
