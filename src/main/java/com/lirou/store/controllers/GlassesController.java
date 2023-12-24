2bbebed94d63ebc4ae83c71c4740fde9bd67bdcc
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-24 11:26:45 -0300

    implements method for getting all glasses

--- src/main/java/com/lirou/store/controllers/GlassesController.java
+++ src/main/java/com/lirou/store/controllers/GlassesController.java
@@ -1,9 +1,9 @@
 package com.lirou.store.controllers;
 
-import com.lirou.store.entities.Glasses;
+import com.lirou.store.DTOs.GlassesDTO;
 import com.lirou.store.models.BaseController;
-import com.lirou.store.models.Message;
 import com.lirou.store.services.GlassesService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +22,7 @@ public class GlassesController extends BaseController {
     // Para a parte admin:
     @GetMapping("/get-all-glasses")
     public ResponseEntity<?> getGlasses() {
-        List<Glasses> glasses;
-
-        return ResponseEntity.ok(new Message("ok"));
+        List<GlassesDTO> glassesDTO = glassesService.getAllGlasses();
+        return ResponseEntity.ok(glassesDTO);
     }
 }
