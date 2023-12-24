0db51d3510bfc4870368a3b85bd8f15d99d28ec9
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-24 11:20:45 -0300

    creates Glasses mapper

---
+++ src/main/java/com/lirou/store/mapper/glasses/GlassesMapper.java
@@ -0,0 +1,28 @@
+package com.lirou.store.mapper.glasses;
+
+import com.lirou.store.DTOs.GlassesDTO;
+import com.lirou.store.entities.Glasses;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GlassesMapper {
+    public GlassesDTO singleToDTO(Glasses glassesEntity) {
+        return new GlassesDTO(
+                glassesEntity.getIdentifier(),
+                glassesEntity.getTitle(),
+                glassesEntity.getPic(),
+                glassesEntity.getInStock(),
+                glassesEntity.getModel(),
+                glassesEntity.getColor(),
+                glassesEntity.getBrand(),
+                glassesEntity.getPrice()
+        );
+    }
+
+    public List<GlassesDTO> severalToDTO (List<Glasses> glassesEntities){
+        return glassesEntities.stream().map(this::singleToDTO).toList();
+    }
+
+}
