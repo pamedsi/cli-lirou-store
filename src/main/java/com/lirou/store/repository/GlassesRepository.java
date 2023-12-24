90729680de7bc67a6fe53646c07eaec6577c4a72
Patrick Melo <patrickabimael@yahoo.com.br>
2023-12-24 11:26:16 -0300

    implements method for getting non deleted glasses

--- src/main/java/com/lirou/store/repository/GlassesRepository.java
+++ src/main/java/com/lirou/store/repository/GlassesRepository.java
@@ -1,9 +1,14 @@
 package com.lirou.store.repository;
 
 import com.lirou.store.entities.Glasses;
+import com.lirou.store.services.GlassesService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface GlassesRepository extends JpaRepository<Long, Glasses> {
+public interface GlassesRepository extends JpaRepository<Glasses, Long> {
+
+    List<Glasses> findAllByDeletedFalse();
 }
