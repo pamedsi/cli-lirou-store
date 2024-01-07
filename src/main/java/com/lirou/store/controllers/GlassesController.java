6b8e4a144ab48f40f764f06f4c91483a7ebdf355
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-07 14:02:58 -0300

    refactors endpoint

--- src/main/java/com/lirou/store/controllers/GlassesController.java
+++ src/main/java/com/lirou/store/controllers/GlassesController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/glasses")
 public class GlassesController{
 
     private final GlassesService glassesService;
