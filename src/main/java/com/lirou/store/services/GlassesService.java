bc998e815f5ab38d2d35b81a7ca018757a9c6e88
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-07 14:01:59 -0300

    removes "lastEditedIn" prop

--- src/main/java/com/lirou/store/services/GlassesService.java
+++ src/main/java/com/lirou/store/services/GlassesService.java
@@ -2,29 +2,23 @@ package com.lirou.store.services;
 
 import com.lirou.store.DTOs.GlassesDTO;
 import com.lirou.store.entities.Glasses;
-import com.lirou.store.mapper.GlassesMapper;
 import com.lirou.store.repository.GlassesRepository;
 
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.NotFoundException;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class GlassesService {
     private GlassesRepository glassesRepository;
-    private GlassesMapper glassesMapper;
-
-    public GlassesService(GlassesRepository glassesRepository, GlassesMapper glassesMapper) {
+    public GlassesService(GlassesRepository glassesRepository) {
         this.glassesRepository = glassesRepository;
-        this.glassesMapper = glassesMapper;
     }
     // Admin:
     public List<GlassesDTO> getAllGlasses() {
         List<Glasses> glassesAsEntity = glassesRepository.findAllByDeletedFalse();
-        return glassesMapper.severalToDTO(glassesAsEntity);
+        return GlassesDTO.severalToDTO(glassesAsEntity);
     }
 
     public void saveNewGlasses(GlassesDTO glassesDTO) {
@@ -35,7 +29,6 @@ public class GlassesService {
     public void updateGlasses(String glassesIdentifier, GlassesDTO changes) {
         Glasses glassesToEdit = glassesRepository.findByIdentifierAndDeletedFalse(glassesIdentifier);
         if (glassesToEdit == null) throw new NotFoundException("Óculos não encontrado!");
-        if (!someChange(glassesToEdit, changes)) return;
 
         glassesToEdit.setTitle(changes.title());
         glassesToEdit.setPic(changes.pic());
@@ -45,18 +38,7 @@ public class GlassesService {
         glassesToEdit.setBrand(changes.brand());
         glassesToEdit.setPrice(changes.price());
 
-        glassesToEdit.setLastEditedIn(LocalDateTime.now());
         glassesRepository.save(glassesToEdit);
-    }
-
-    private Boolean someChange(Glasses glassesToEdit, GlassesDTO changes) {
-        return !Objects.equals(glassesToEdit.getTitle(), changes.title()) ||
-                !Objects.equals(glassesToEdit.getPic(), changes.pic()) ||
-                !Objects.equals(glassesToEdit.getModel(), changes.model()) ||
-                !Objects.equals(glassesToEdit.getFrame(), changes.frame()) ||
-                !Objects.equals(glassesToEdit.getColor(), changes.color()) ||
-                !Objects.equals(glassesToEdit.getBrand(), changes.brand()) ||
-                glassesToEdit.getPrice().compareTo(changes.price()) != 0;
     }
 
     public String removeGlasses(String glassesIdentifier) {
