319fedf5405ba94c29e59312dee9713700fe54bc
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-08 18:20:23 -0300

    implements endpoint for sending shipping to superfrete

--- src/main/java/com/lirou/store/controllers/ShippingController.java
+++ src/main/java/com/lirou/store/controllers/ShippingController.java
@@ -1,18 +1,18 @@
 package com.lirou.store.controllers;
 
-import com.lirou.store.DTOs.bodyForCalculateShipping.FromAndTo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.lirou.store.DTOs.bodyForCalculateShipping.FromAndToDTO;
+import com.lirou.store.DTOs.shippingInfToSendToSuperFrete.ShippingInfToSendToSuperFreteDTO;
 import com.lirou.store.services.SuperFreteService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/shipping")
 public class ShippingController {
 
     private final SuperFreteService superFreteService;
@@ -21,9 +21,14 @@ public class ShippingController {
         this.superFreteService = superFreteService;
     }
 
-    @GetMapping("/calculate-shipping")
-    public ResponseEntity<?> calculateShipping(@RequestBody FromAndTo postalCodes) throws IOException {
+    @GetMapping("/calculate")
+    public ResponseEntity<?> calculateShipping(@RequestBody FromAndToDTO postalCodes) throws IOException {
         ResponseEntity<?> response = superFreteService.calculateShipping(postalCodes.from(), postalCodes.to());
+        return ResponseEntity.ok(response.getBody());
+    }
+    @PostMapping("/send-to-superfrete")
+    public ResponseEntity<?> sendShippingToSuperFrete(@RequestBody ShippingInfToSendToSuperFreteDTO body) throws JsonProcessingException {
+        ResponseEntity<?> response = superFreteService.sendShippingToSuperFrete(body);
         return ResponseEntity.ok(response.getBody());
     }
 }
