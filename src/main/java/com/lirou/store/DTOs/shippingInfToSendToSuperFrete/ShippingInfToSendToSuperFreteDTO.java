a1770598358070d9da8c8692977de2903a62a1c6
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-08 18:20:03 -0300

    refactors DTO

---
+++ src/main/java/com/lirou/store/DTOs/shippingInfToSendToSuperFrete/ShippingInfToSendToSuperFreteDTO.java
@@ -0,0 +1,25 @@
+package com.lirou.store.DTOs.shippingInfToSendToSuperFrete;
+
+import java.util.List;
+
+public record ShippingInfToSendToSuperFreteDTO(
+        String platform,
+        AddressDTO from,
+        AddressDTO to,
+        Number service,
+        List<Product> products,
+        Volumes volumes
+){}
+
+record Product (
+        String name,
+        String quantity,
+        String unitary_value
+){}
+
+record Volumes (
+        Number height,
+        Number width,
+        Number length,
+        Number weight
+){}
