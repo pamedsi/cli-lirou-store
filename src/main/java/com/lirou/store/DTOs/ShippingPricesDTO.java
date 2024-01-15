b32d44f247f3c6affd50d6824dd9feee10410bef
Patrick Melo <patrickabimael@yahoo.com.br>
2024-01-14 22:41:21 -0300

    creates shipping price DTO

--- src/main/java/com/lirou/store/DTOs/ShippingPricesDTO.java
+++ src/main/java/com/lirou/store/DTOs/ShippingPricesDTO.java
@@ -1,3 +1,21 @@
 package com.lirou.store.DTOs;
 
-public record ShippingPricesDTO() {}
+import org.springframework.format.annotation.NumberFormat;
+
+import java.math.BigDecimal;
+
+public record ShippingPricesDTO(
+        String id,
+        String name,
+        Number delivery_time,
+        @NumberFormat
+        BigDecimal price,
+        CompanyInfo company
+
+) {}
+
+record CompanyInfo (
+        Number id,
+        String name,
+        String picture
+){}
