f3395fc54a3abcfb7e982ca8a22efa53141de47d
Patrick Melo <patrickabimael@yahoo.com.br>
2024-02-05 08:47:24 -0300

    creates user entity

---
+++ src/main/java/com/lirou/store/domain/entities/UserEntity.java
@@ -0,0 +1,36 @@
+package com.lirou.store.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Table
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private LocalDate birthDate;
+    @Column
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime userSince;
+    @Column
+    @Setter(AccessLevel.NONE)
+    private String CPF;
+
+    public long getAge() {
+        return ChronoUnit.DAYS.between(this.birthDate, LocalDate.now());
+    }
+}
