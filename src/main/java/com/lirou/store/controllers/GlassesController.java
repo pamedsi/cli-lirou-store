c4e905778069fae2ddd1a2f1dbc5c1297c8377d9
Patrick Melo <88412880+pamedsi@users.noreply.github.com>
2024-02-20 22:42:44 -0300

    Revert "Test"

--- src/main/java/com/lirou/store/controllers/GlassesController.java
+++ src/main/java/com/lirou/store/controllers/GlassesController.java
@@ -1,6 +1,5 @@
 package com.lirou.store.controllers;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -16,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lirou.store.domain.DTOs.GlassesDTO;
-import com.lirou.store.exceptions.NameExisteInDatabaseException;
+import com.lirou.store.DTOs.GlassesDTO;
 import com.lirou.store.exceptions.NotFoundException;
 import com.lirou.store.models.GlassesAvailability;
 import com.lirou.store.models.Message;
@@ -25,12 +23,10 @@ import com.lirou.store.services.GlassesService;
 import com.lirou.store.validation.identifierValidator.ValidIdentifier;
 
 import jakarta.validation.Valid;
-import lombok.extern.log4j.Log4j2;
 
 
 @RestController
 @RequestMapping("/api/glasses")
-@Log4j2
 public class GlassesController{
 
     private final GlassesService glassesService;
@@ -42,47 +38,35 @@ public class GlassesController{
     // Para a parte admin:
     @GetMapping
     public ResponseEntity<Page<GlassesDTO>> getGlasses(@PageableDefault(page = 0, size = 24, direction = Sort.Direction.ASC, sort = { "title" }) Pageable pageable) {
-        log.info("[Inicia] GlassesService - getAllGlasses()");
         Page<GlassesDTO> glassesDTO = glassesService.getAllGlasses(pageable);
-        log.info("[Finaliza] GlassesService - getAllGlasses()");
         return ResponseEntity.ok(glassesDTO);
     }
     @GetMapping("/{identifier}")
     public ResponseEntity<GlassesDTO> findSingleGlasses(@PathVariable("identifier") @ValidIdentifier String identifier) throws NotFoundException {
-        log.info("[Inicia] GlassesService - findSingleGlasses()");
         GlassesDTO glassesDTO = glassesService.findGlassesByIdentifier(identifier);
-        log.info("[Finaliza] GlassesService - findSingleGlasses()");
         return ResponseEntity.ok(glassesDTO);
     }
     @PostMapping
-    public ResponseEntity<Message> postGlasses(@RequestBody @Valid GlassesDTO glassesDTO) throws NameExisteInDatabaseException {
-        log.info("[Inicia] GlassesService - saveNewGlasses()");
+    public ResponseEntity<Message> postGlasses(@RequestBody @Valid GlassesDTO glassesDTO) {
         glassesService.saveNewGlasses(glassesDTO);
-        log.info("[Finaliza] GlassesService - saveNewGlasses()");
         return ResponseEntity.status(201).body(new Message(glassesDTO.title() + " salvo!"));
     }
 
     @PutMapping("/{identifier}")
     public ResponseEntity<?> putGlasses(@RequestBody @Valid GlassesDTO glassesDTO, @PathVariable("identifier") String glassesIdentifier) throws NotFoundException {
-        log.info("[Inicia] GlassesService - updateGlasses()");
         glassesService.updateGlasses(glassesIdentifier, glassesDTO);
-        log.info("[Finaliza] GlassesService - updateGlasses()");
         return ResponseEntity.ok(new Message("Atualização feita com sucesso!"));
     }
 
     @DeleteMapping("/{identifier}")
     public ResponseEntity<?> deleteGlasses(@PathVariable("identifier") String glassesIdentifier) throws NotFoundException {
-        log.info("[Inicia] GlassesService - removeGlasses()");
         String titleOfDeletedGlasses = glassesService.removeGlasses(glassesIdentifier);
-        log.info("[Finaliza] GlassesService - removeGlasses()");
         return ResponseEntity.ok(new Message(titleOfDeletedGlasses + " deletado!"));
     }
   
     @PatchMapping("/{identifier}")
-    public ResponseEntity<?> changeAvailability(@PathVariable("identifier") String glassesIdentifier, @RequestBody @Valid GlassesAvailability availability) throws NotFoundException {
-        log.info("[Inicia] GlassesService - changeAvailability()");
+    public ResponseEntity<?> changeAvailability(@PathVariable("identifier") String glassesIdentifier, @RequestBody GlassesAvailability availability) throws NotFoundException {
         String availableOrNot = glassesService.changeAvailability(glassesIdentifier, availability.available());
-        log.info("[Finaliza] GlassesService - changeAvailability()");
         return ResponseEntity.ok(new Message("Óculos " + availableOrNot));
     }
 }
--- src/test/java/com/lirou/store/services/GlassesServiceTest.java
+++ src/test/java/com/lirou/store/services/GlassesServiceTest.java
@@ -1,23 +1,17 @@
 package com.lirou.store.services;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.lirou.store.domain.DTOs.GlassesDTO;
-import com.lirou.store.domain.entities.Glasses;
-import com.lirou.store.exceptions.NameExisteInDatabaseException;
+import com.lirou.store.entities.Glasses;
 import com.lirou.store.exceptions.NotFoundException;
 import com.lirou.store.repository.GlassesRepository;
 
@@ -33,116 +27,7 @@ public class GlassesServiceTest {
 	@Mock
 	private Glasses glasses;
 	
-	private static GlassesDTO glassesdto;
 	
-	@Captor
-	private ArgumentCaptor<Glasses> glassesCaptor;
-	
-	@BeforeEach
-	void beforeach() {
-		glassesdto = new GlassesDTO(null, "glasses", null, true, 0, null, null, "yellow", null, BigDecimal.valueOf(10.0));
-
-	}
-
-	
-	//Testando metodo saveNewGlasses **************************************************
-	@Test
-	@DisplayName("deveria persistir no banco")
-	void deveriaSalvarNoBanco() throws NameExisteInDatabaseException {
-		//arrange
-		BDDMockito.given(glassesRepository.existsByTitleAndDeletedFalse(glassesdto.title())).willReturn(false);
-		
-		//act
-		glassesService.saveNewGlasses(glassesdto);
-		
-		//assert
-		BDDMockito.then(glassesRepository).should().save(glassesCaptor.capture());
-		Glasses glasses = glassesCaptor.getValue();
-		
-		Assertions.assertEquals(glassesdto.title(), glasses.getTitle());
-		Assertions.assertEquals(glassesdto.price(), glasses.getPrice());
-		Assertions.assertEquals(glassesdto.available(), glasses.getAvailable());
-		Assertions.assertEquals(glassesdto.color(), glasses.getColor());
-	}
-	
-	@Test
-	@DisplayName("deveria dar error persistir no banco  e lançar exception")
-	void deveriaDarErrorAoSalvarNoBanco() {
-		//arrange
-		BDDMockito.given(glassesRepository.existsByTitleAndDeletedFalse(glassesdto.title())).willReturn(true);
-		
-		//assert && act
-		Assertions.assertThrows(NameExisteInDatabaseException.class,() -> glassesService.saveNewGlasses(glassesdto));
-
-	}
-	
-	//Testando metodo updateGlasses**************************************************
-	@Test
-	@DisplayName("deveria conseguir editar o produto")
-	void editarProduto() throws NotFoundException {
-		//arrange
-		BDDMockito.given(glassesRepository.findByIdentifierAndDeletedFalse(glassesdto.identifier())).willReturn(Optional.of(new Glasses()));
-		
-		//act
-		glassesService.updateGlasses(glassesdto.identifier(),glassesdto);
-				
-		//assert
-		BDDMockito.then(glassesRepository).should().save(glassesCaptor.capture());
-		Glasses glasses = glassesCaptor.getValue();
-		
-		Assertions.assertEquals(glassesdto.title(), glasses.getTitle());
-		Assertions.assertEquals(glassesdto.price(), glasses.getPrice());
-		Assertions.assertEquals(glassesdto.color(), glasses.getColor());
-		Assertions.assertEquals(glassesdto.brand(), glasses.getBrand());
-		Assertions.assertEquals(glassesdto.pic(), glasses.getPic());
-		Assertions.assertEquals(glassesdto.frame(), glasses.getFrame());
-		Assertions.assertEquals(glassesdto.model(), glasses.getModel());
-		
-	}
-	
-	@Test
-	@DisplayName("Não deveria conseguir editar o produto")
-	void nãoDeveEditarProduto() throws NotFoundException {
-		//arrange
-		BDDMockito.given(glassesRepository.findByIdentifierAndDeletedFalse(glassesdto.identifier())).willReturn(Optional.empty());
-		
-		//act		
-		//assert
-		Assertions.assertThrows(NotFoundException.class,() -> glassesService.updateGlasses(glassesdto.identifier(),glassesdto));
-
-	}
-	
-	//Testando metodo remove**************************************************
-		@Test
-		@DisplayName("deveria conseguir remover o produto")
-		void removerProduto() throws NotFoundException {
-			//arrange
-			BDDMockito.given(glassesRepository.findByIdentifierAndDeletedFalse(glassesdto.identifier())).willReturn(Optional.of(new Glasses()));
-			
-			//act
-			glassesService.removeGlasses(glassesdto.identifier());
-					
-			//assert
-			BDDMockito.then(glassesRepository).should().save(glassesCaptor.capture());
-			Glasses glasses = glassesCaptor.getValue();
-			
-			Assertions.assertEquals(true,glasses.getDeleted());
-		}
-		
-		@Test
-		@DisplayName("Não deveria conseguir remover o produto, deve lançar exception")
-		void nãoDeveRemoverProduto() throws NotFoundException {
-			//arrange
-			BDDMockito.given(glassesRepository.findByIdentifierAndDeletedFalse(glassesdto.identifier())).willReturn(Optional.empty());
-			
-			//act		
-			//assert
-			Assertions.assertThrows(NotFoundException.class,() -> glassesService.updateGlasses(glassesdto.identifier(),glassesdto));
-
-		}
-	
-	
-	//Testando metodo changeAvailability **************************************************
 	@Test()
 	@DisplayName("deveria retornar que o produto está disponivel")
 	void conferirSeEstaDisponivel() throws NotFoundException {
