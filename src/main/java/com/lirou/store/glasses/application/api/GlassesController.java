4ad6eebd372b4f87916b3f1425543f1bcfb7d75d
Patrick Melo <patrickabimael@yahoo.com.br>
2024-04-05 13:49:04 -0300

    implements interface for glasses controller

--- src/main/java/com/lirou/store/glasses/application/api/GlassesController.java
+++ src/main/java/com/lirou/store/glasses/application/api/GlassesController.java
@@ -5,17 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lirou.store.handler.exceptions.NotFoundException;
 import com.lirou.store.models.Message;
 import com.lirou.store.glasses.application.service.GlassesService;
 import com.lirou.store.shared.validation.identifierValidator.ValidIdentifier;
@@ -25,7 +20,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/glasses")
-public class GlassesController{
+public class GlassesController implements GlassesAPI{
 
     private final GlassesService glassesService;
 
@@ -33,35 +28,34 @@ public class GlassesController{
         this.glassesService = glassesService;
     }
 
-    // Para a parte admin:
-    @GetMapping
+    @Override
     public ResponseEntity<Page<GlassesDTO>> getGlasses(@PageableDefault(size = 24, direction = Sort.Direction.ASC, sort = { "title" }) Pageable pageable) {
         Page<GlassesDTO> glassesDTO = glassesService.getAllGlasses(pageable);
         return ResponseEntity.ok(glassesDTO);
     }
-    @GetMapping("/{identifier}")
-    public ResponseEntity<GlassesDTO> findSingleGlasses(@PathVariable("identifier") @ValidIdentifier String identifier) throws NotFoundException {
+    @Override
+    public ResponseEntity<GlassesDTO> findSingleGlasses(@PathVariable("identifier") @ValidIdentifier String identifier) {
         GlassesDTO glassesDTO = glassesService.getGlassesWithIdentifier(identifier);
         return ResponseEntity.ok(glassesDTO);
     }
-    @PostMapping
+    @Override
     public ResponseEntity<Message> postGlasses(@RequestBody @Valid GlassesDTO glassesDTO) {
         glassesService.saveNewGlasses(glassesDTO);
         return ResponseEntity.status(201).body(new Message(glassesDTO.title() + " salvo!"));
     }
-    @PutMapping("/{identifier}")
-    public ResponseEntity<?> putGlasses(@RequestBody @Valid GlassesDTO glassesDTO, @PathVariable("identifier") String glassesIdentifier) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> putGlasses(@RequestBody @Valid GlassesDTO glassesDTO, @PathVariable("identifier") String glassesIdentifier) {
         glassesService.editGlasses(glassesIdentifier, glassesDTO);
-        return ResponseEntity.ok(new Message("Atualização feita com sucesso!"));
+        return ResponseEntity.status(202).body(new Message("Atualização feita com sucesso!"));
     }
-    @DeleteMapping("/{identifier}")
-    public ResponseEntity<?> deleteGlasses(@PathVariable("identifier") String glassesIdentifier) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> deleteGlasses(@PathVariable("identifier") String glassesIdentifier) {
         String titleOfDeletedGlasses = glassesService.removeGlasses(glassesIdentifier);
         return ResponseEntity.ok(new Message(titleOfDeletedGlasses + " deletado!"));
     }
-    @PatchMapping("/{identifier}")
-    public ResponseEntity<?> changeAvailability(@PathVariable("identifier") String glassesIdentifier, @RequestBody @Valid GlassesAvailability availability) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> changeAvailability(@PathVariable("identifier") String glassesIdentifier, @RequestBody @Valid GlassesAvailability availability)  {
         String availableOrNot = glassesService.changeAvailability(glassesIdentifier, availability.available());
-        return ResponseEntity.ok(new Message("Óculos " + availableOrNot));
+        return ResponseEntity.status(202).body(new Message("Óculos " + availableOrNot));
     }
 }
