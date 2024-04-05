5ee2f774f8c010194daabee757e1532fa9011c87
Patrick Melo <patrickabimael@yahoo.com.br>
2024-04-05 13:00:26 -0300

    implements interface for address controller

---
+++ src/main/java/com/lirou/store/address/application/api/AddressAPI.java
@@ -0,0 +1,31 @@
+package com.lirou.store.address.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/address")
+public interface AddressAPI {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    ResponseEntity<List<UserAddressDTO>> getAddresses(@RequestHeader("Authorization") String token);
+    @GetMapping("/{addressIdentifier}")
+    @ResponseStatus(HttpStatus.OK)
+    ResponseEntity<UserAddressDTO> getAddress(@RequestHeader("Authorization") String token, @PathVariable("addressIdentifier") String addressIdentifier);
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    ResponseEntity<?> addNewAddress(@RequestHeader("Authorization") String token, @RequestBody UserAddressDTO addressDTO);
+
+    @PutMapping("/{addressIdentifier}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    ResponseEntity<?> editAddress(@RequestHeader("Authorization") String token, @RequestBody UserAddressDTO addressDTO, @PathVariable("addressIdentifier") String addressIdentifier);
+
+    @DeleteMapping("/{addressIdentifier}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    ResponseEntity<?> deleteAddress(@RequestHeader("Authorization") String token, @PathVariable("addressIdentifier") String addressIdentifier);
+}
--- src/main/java/com/lirou/store/address/application/api/AddressController.java
+++ src/main/java/com/lirou/store/address/application/api/AddressController.java
@@ -1,6 +1,5 @@
 package com.lirou.store.address.application.api;
 
-import com.lirou.store.handler.exceptions.NotFoundException;
 import com.lirou.store.address.application.service.AddressApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -15,42 +14,42 @@ import java.util.List;
 @RequestMapping("/api/address")
 @Log4j2
 @RequiredArgsConstructor
-public class AddressController {
+public class AddressController implements AddressAPI{
     private final AddressApplicationService addressApplicationService;
 
-    @GetMapping
-    public ResponseEntity<List<UserAddressDTO>> getAddresses(@RequestHeader("Authorization") String token) {
+    @Override
+    public ResponseEntity<List<UserAddressDTO>> getAddresses(String token) {
         log.info("[starts] AddressService - getAllAddresses()");
         List<UserAddressDTO> addresses = addressApplicationService.getAllAddresses(token);
         log.info("[ends] AddressService - getAllGlasses()");
         return ResponseEntity.ok(addresses);
     }
-    @GetMapping("/{addressIdentifier}")
-    public ResponseEntity<UserAddressDTO> getAddress(@RequestHeader("Authorization") String token, @PathVariable("addressIdentifier") String addressIdentifier) {
+    @Override
+    public ResponseEntity<UserAddressDTO> getAddress(String token,String addressIdentifier) {
         log.info("[starts] AddressService - getAllAddresses()");
         UserAddressDTO addresses = addressApplicationService.getAddressWithIdentifier(token, addressIdentifier);
         log.info("[ends] AddressService - getAllGlasses()");
         return ResponseEntity.ok(addresses);
     }
-    @PostMapping
-    public ResponseEntity<?> addNewAddress(@RequestHeader("Authorization") String token, @RequestBody UserAddressDTO addressDTO) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> addNewAddress(String token, UserAddressDTO addressDTO) {
         log.info("[starts] AddressController - addNewAddress()");
         addressApplicationService.addNewAddress(token, addressDTO);
         log.info("[ends] AddressController - addNewAddress()");
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-    @PostMapping("/{identifier}")
-    public ResponseEntity<?> editAddress(@RequestHeader("Authorization") String token, @RequestBody UserAddressDTO addressDTO, @PathVariable("identifier") String addressIdentifier) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> editAddress(String token, @RequestBody UserAddressDTO addressDTO, String addressIdentifier) {
         log.info("[starts] AddressController - editAddress()");
         addressApplicationService.editAddress(token, addressDTO, addressIdentifier);
         log.info("[ends] AddressController - editAddress()");
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
-    @DeleteMapping("/{identifier}")
-    public ResponseEntity<?> deleteAddress(@RequestHeader("Authorization") String token, @PathVariable("identifier") String addressIdentifier) throws NotFoundException {
+    @Override
+    public ResponseEntity<?> deleteAddress(String token, String addressIdentifier) {
         log.info("[starts] AddressController - deleteAddress()");
         addressApplicationService.deleteAddress(token, addressIdentifier);
         log.info("[ends] AddressController - deleteAddress()");
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
